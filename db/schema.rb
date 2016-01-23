418a71ffa1d5d7b3b229bc83f1002aa2a91028f7
Ovi Calvo & Alice Malone <pair+0viwan.malicemalone@devbootcamp.com>
2016-01-23 15:31:14 -0800

    restores proper formatting to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160123205703) do
+ActiveRecord::Schema.define(version: 20160123232805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +24,5 @@ ActiveRecord::Schema.define(version: 20160123205703) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
   end
-
 
 end
