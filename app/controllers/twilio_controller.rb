2fcc59877d02e4985773fdf1ab86993a1c3256d9
Ovi Calvo & Alice Malone <pair+0viwan.malicemalone@devbootcamp.com>
2016-01-23 17:25:53 -0800

    fixes conflicts

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,20 +1,16 @@
 class TwilioController < ApplicationController
 
   def index
-
+    Twilio.
   end
 
   def new
-    @user = User.new
+
   end
 
   def create
-    info = params[:user]
-    @user = User.create(phone: info["phone"], bus_num: info["bus_num"], stop_id: info["stop_id"], stopping_point: info["stopping_point"])
-    @bus_num = @user.bus_num
-    #make API call to 511 to get bus status update
 
-    #text user
-    User.text_update(@user.phone, '5')
   end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,5 @@
-require 'twilio-ruby'
-
 class User < ActiveRecord::Base
   validates :phone, presence: true
   validates :bus_num, presence: true
   validates :stop_id, presence: true
-
-
-  def self.text_update(phone, est_time)
-    @client = Twilio::REST::Client.new(ENV['TWILIO_API_KEY'],  ENV['TWILIO_API_SECRET'])
-
-    @client.messages.create(
-    from: '+16693421992',
-    to: '+1' + phone,
-    body: 'Hey there! Your bus will arrive in ' + est_time + ' minutes!'
-    )
-  end
-
 end
-
-
