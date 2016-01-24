2c4a1cecd15d34e749e2d171bbf66594ee7383cc
Jenna Andersen <jnicholea@gmail.com>
2016-01-23 19:16:22 -0800

    adjust 511 api call to return all stops along route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
 
 	def index
+		@stops = Page.all_route_stops
 		@result = Page.all_route_stops
 	end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -3,19 +3,19 @@ class Page < ActiveRecord::Base
   validates :bus_num, presence: true
   validates :stop_id, presence: true
 
+
+
   def self.all_route_stops
   client = SFBATransitAPI.client ENV['511_AUTH_TOKEN']
 
-	agencies = client.get_stops_for_route(agency_name: "SF-MUNI", route_code: "19", route_direction_code: "Outbound")
+	agencies = client.get_stops_for_route(agency_name: "SF-MUNI", route_code: "19", route_direction_code: "Inbound")
 
 	@stops = agencies[0]["routes"][0]["route_directions"][0]["stops"]
 
 	my_stop = @stops.find { |stop| stop["name"] =~ /Polk.+Sutter/ }
 
 	p @result = client.get_next_departures_by_stop_code("16002")[0]["routes"][0]["route_directions"][0]["stops"][0]["departure_times"]
-  p "Jenna just found the next Muni bus to leave from the Polk and Sutter station"
-  @result
-  @stops
   end
 
 end
+
