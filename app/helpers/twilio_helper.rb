1e6d3610e099829b7878f9e543594afdd399b468
Ovi Calvo & Alice Malone <pair+0viwan.malicemalone@devbootcamp.com>
2016-01-23 20:35:27 -0800

    adds pseudocode for methods that will handle Google Maps api calls

--- app/helpers/twilio_helper.rb
+++ app/helpers/twilio_helper.rb
@@ -1,14 +1,26 @@
 module TwilioHelper
-#   def ping_map(api)
+  # def ping_map_api
 
-#   end
+  # end
 
 
-#   def looped_ping(bus_num, destination)
-#     #do first api call here using bus_num and destination
-#     # route_array  = result of api call
-#     until bus is 3 stops away from destination
-#       ping_map_api
+  # def looped_ping(bus_num, starting_point, destination)
+  #   #do first api call here using bus_num and destination
+  #   # route_array  = ping_map_api
+  #  until bus is less than 3 stops from route_array[-1]
+  #     sleep(2.minutes)
+  #     result = ping_map_api
+  #     compare result against route_array
+  #   end
 
-#   end
+  #   until bus is 4 or fewer stops away from destination
+  #       ping_map_api
+  #       sleep(10.seconds)
+  #       result = ping_map_api
+  #       if result.length <= 3
+  #         User.text_update(@user.phone, "You're almost to your destination! Watch the digital screen up front and pull on the yellow cord when you see your stop.")
+  #         break
+  #       end
+  #   end
+  # end
 end
