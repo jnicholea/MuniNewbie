3381f45ec26822c9d7ae67d58e71e4153fd3e99d
Sarah A <sarah.alsinan@gmail.com>
2016-03-04 09:10:07 -0800

    Update page.rb
    Modifies spacing and indentation

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,20 +2,13 @@ class Page < ActiveRecord::Base
   validates :phone, presence: true
   validates :bus_num, presence: true
   validates :stop_id, presence: true
-
-
-
+  
   def self.all_route_stops
-  client = SFBATransitAPI.client ENV['511_AUTH_TOKEN']
-
-	agencies = client.get_stops_for_route(agency_name: "SF-MUNI", route_code: "19", route_direction_code: "Inbound")
-
-	@stops = agencies[0]["routes"][0]["route_directions"][0]["stops"]
-
-	my_stop = @stops.find { |stop| stop["name"] =~ /Polk.+Sutter/ }
-
-	p @result = client.get_next_departures_by_stop_code("16002")[0]["routes"][0]["route_directions"][0]["stops"][0]["departure_times"]
+    client = SFBATransitAPI.client ENV['511_AUTH_TOKEN']
+    agencies = client.get_stops_for_route(agency_name: "SF-MUNI", route_code: "19", route_direction_code: "Inbound")
+    @stops = agencies[0]["routes"][0]["route_directions"][0]["stops"]
+    my_stop = @stops.find { |stop| stop["name"] =~ /Polk.+Sutter/ }
+    p @result = client.get_next_departures_by_stop_code("16002")[0]["routes"][0]["route_directions"][0]["stops"][0]["departure_times"]
   end
-
 end
 
